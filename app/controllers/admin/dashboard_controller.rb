eb5c3a403b69ab789128b331007022350b8530e9
Isaiah Mutekanga <isaiahmutekanga@trentu.ca>
2022-01-05 18:21:36 +0000

    Added security

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class Admin::DashboardController < ApplicationController
+
+       http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+
+           #http_basic_authenticate_with name: "jungle", password: "book"
+
+
   def show
   end
 end
